6abb45eb855873b51efbe2045cbbbac51a0b8c97
mezbahalam <mezbahalam26@gmail.com>
2014-02-21 13:06:47 +0600

    search6

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,5 +2,5 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable and :omniauthable ,:confirmable,
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 end
